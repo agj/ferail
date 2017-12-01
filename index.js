903b7d8797a508739088a0e37f82f240756b2c50
agj <ale@agj.cl>
2017-12-01 18:40:22 -0300

    Rewrote script. Now outputs VTT, and supports ruby for it (for ASS the ruby doesn't get formatted).

--- index.js
+++ index.js
@@ -1,86 +1,71 @@
 
-
 const R = require('ramda');
 const fs = require('fs-promise');
 const glob = require('glob-promise');
-const assParser = require('ass-parser');
-const assStringify = require('ass-parser');
+const xre = require('xregexp');
 require('dot-into').install();
 
+const log = R.tap(console.log);
 const whenAll = Promise.all.bind(Promise);
-const whenAllObj = obj =>
-	Object.keys(obj)
-	.into(R.reduce((acc, prop) => whenAll([acc, obj[prop]]).then(([accResult, thisResult]) => {
-		accResult[prop] = thisResult;
-		return accResult;
-	}), Promise.resolve({})));
-const getLanguage = R.replace(/input\/text\/(.+)\.txt/, '$1');
 const readFile = filename => fs.readFile(filename, { encoding: 'utf8' });
 const writeFile = (filename, data) => fs.writeFile(filename, data, { encoding: 'utf8' });
-
-const getScripts = scriptsGlob =>
-	glob(scriptsGlob)
-	.then(R.map(filename => ({ language: getLanguage(filename), script: readFile(filename).then(R.split('\n')) })))
-	.then(R.map(whenAllObj))
-	.then(whenAll);
-const getTimeScripts = (timedFilename, scriptsGlob) =>
-	readFile(timedFilename)
-	.then(assParser)
-	.then(timed =>
-		getScripts(scriptsGlob)
-		.then(R.map(script => ({ timed: timed, script: script.script, language: script.language })))
-	);
-const isSectionEvents = R.propEq('section', 'Events');
-R.propEq('key', 'Dialogue')
-const applyTimes = ({ language, timed, script }) =>
-	({ language: language, subtitle:
-		timed.map(R.when(isSectionEvents, evts =>
-			R.merge(evts, { body:
-				evts.body
-			})
-		))
-		// R.merge(timed, { events:
-		// 	R.zip(timed, script)
-		// 	.into(R.map(R.apply((ev, line) => R.merge(ev, { text: line }))))
-		// })
-	});
-
-
-// const getFilePairs = (timedFilename, scriptsGlob) =>
-// 	[
-// 		readFile(timedFilename),
-// 		glob(scriptsGlob)
-// 			.then(R.map(filename => [getLanguage(filename), readFile(filename)]))
-// 			.then(R.map(whenAll))
-// 			.then(whenAll)
-// 	]
-// 	.into(whenAll);
-
-// const re = /^(Dialogue: \d,\d:\d\d:\d\d\.\d\d,\d:\d\d:\d\d\.\d\d,Default,,\d,\d,\d,,)(.*)$/;
-// const timeText = (text, times) =>
-// 	R.reduce(
-// 		([result, lines], timed) =>
-// 			R.test(re, timed)
-// 			? [R.append(R.replace(re, '$1' + R.head(lines), timed), result),
-// 			   R.tail(lines)]
-// 			: [R.append(timed, result),
-// 			   lines],
-// 		[[], text.split('\n')],
-// 		times.split('\n')
-// 	)
-// 	.into(R.nth(0))
-// 	.join('\n');
+const getLanguage = R.replace(/input\/text\/(.+)\.txt/, '$1');
+const timedLineRE = /^(Dialogue: \d,(\d:\d\d:\d\d\.\d\d),(\d:\d\d:\d\d\.\d\d),Default,,\d,\d,\d,,)(.*)$/;
+const isTimedLine = R.test(timedLineRE);
+const toTimes = subtitle =>
+	subtitle.split('\n')
+	.filter(isTimedLine)
+	.map(line => ({
+		start: line.replace(timedLineRE, '$2'),
+		end:   line.replace(timedLineRE, '$3'),
+	}));
+const replaceLines = R.curry((sub, lines) =>
+	R.reduce(
+		([result, remaining], subLine) =>
+			isTimedLine(subLine)
+			? [R.append(subLine.replace(timedLineRE, '$1' + R.head(remaining)), result),
+			   R.tail(remaining)]
+			: [R.append(subLine, result),
+			   remaining],
+		[[], lines],
+		sub.split('\n')
+	)
+	.into(R.head)
+	.join('\n')
+);
+const toVTT = R.curry((times, lines) =>
+	R.zip(times, lines)
+	.map(([time, line]) => formatVTTLine(time, line))
+	.join('\n')
+	.into(sub => 'WEBVTT\n\n' + sub)
+);
+const formatVTTLine = (time, line) =>
+	`${ time.start } --> ${ time.end }\n` +
+	processLineVTT(line) + '\n';
+const splitLines = script => script.split(/\n(?!  )/).map(R.trim);
+const processLineASS = line => line.replace(/\n  /g, '\\N');
+const rubyRE = xre('(?:｜)?([^｜]+)《(.+)》', 'g');
+log(rubyRE)
+const processLineVTT = line => line.replace(rubyRE, '<ruby>$1<rt>$2</rt></ruby>');
 
 
-getTimeScripts('input/times/timed.ass', 'input/text/*.txt')
-.then(R.map(applyTimes))
-.then(R.map(sub => writeFile(`output/${ sub.language }.ass`, ass.Script.prototype.toAss.call(sub.subtitle))))
+(async () => {
+	const timed = await readFile('input/times/timed.ass');
+	const times = toTimes(timed);
+	const scriptFilenames = await glob('input/text/*.txt');
+	const scripts = (
+			await scriptFilenames
+			.map(async filename => [getLanguage(filename), splitLines(await readFile(filename))])
+			.into(whenAll)
+		)
+		.into(R.fromPairs);
+	const assSubtitles = scripts
+		.into(R.map(R.map(processLineASS)))
+		.into(R.map(replaceLines(timed)));
+	const vttSubtitles = scripts
+		.into(R.map(toVTT(times)));
 
-// .then(([times, texts]) => texts.map(([lang, text]) => [lang, timeText(text, times)]))
-// .then(R.map(([lang, subs]) => fs.writeFile('output/' + lang + '.ass', subs, { encoding: 'utf8' })))
-// .then(Promise.all.bind(Promise))
-// .then(R.always('Finished!'))
-
-// .then(console.log)
-.catch(e => console.log(e.stack));
+	assSubtitles.into(R.forEachObjIndexed((ass, lang) => writeFile(`output/ass/${ lang }.ass`, ass)));
+	vttSubtitles.into(R.forEachObjIndexed((vtt, lang) => writeFile(`output/vtt/${ lang }.vtt`, vtt)));
+})();
 
--- src/translate.js
+++
@@ -1,4 +0,0 @@
-
-const replaceText = (ass, text) => {
-
-}
